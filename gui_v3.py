f0ebeb49389edd3ceb8008de26e8c6f03b62d873
JollyWaverley <150859838+JollyWaverley@users.noreply.github.com>
2026-02-19 13:39:37 +1300

    Add files via upload

--- gui_v3.py
+++ gui_v3.py
@@ -1,4 +1,4 @@
-from lib2to3.pytree import convert
+
 from tkinter import*
 
 
@@ -106,10 +106,38 @@ class Converter:
             self.answer_error.config(text=f"Converting {to_convert} F° to C°")
 
 
+def round_ans(val):
+
+    """Round the tempretures to teh nearest degree
+    :param val Number would be rounded
+    :return: Number to nearest degree"""
+
+    var_rounded = (val * 2 + 1) // 2
+    return "{:.0f}".format(var_rounded)
+
+def to_celsius(to_convert):
+    """Converts from f to c
+    param to_convert : temprature to be converted into f
+    :return: Converted in c"""
+
+    answer = (to_convert - 32) * 5 / 9
+    return round_ans(answer)
+
+
+def to_fahrenheit(to_convert):
+    """
+    Converts from c to f
+    : param to_ convert : temperature to be in c
+    : return : Converted temperature in f
+    """
+    answer = to_convert * 1.8 + 32
+    return  round_ans(answer)
 
 
 
-#main routine
+
+
+# Main Routine / Testing starts here
 if __name__ == "__main__":
     root = Tk()
     root.title("temperature converter")
