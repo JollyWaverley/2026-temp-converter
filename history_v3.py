30dae715b1b5792b007d131ee861820c4acb9666
JollyWaverley <150859838+JollyWaverley@users.noreply.github.com>
2026-03-04 09:55:47 +1300

    edited history and started vid 14

--- history_v3.py
+++ history_v3.py
@@ -16,7 +16,7 @@ class Converter:
 
         self.all_calculations_list = ['10.0 °F is -12°C', '20.0 °F is -7°C',
                                       '30.0 °F is -1°C', '40.0 °F is 4°C',
-                                      '50.0 °F is 10°C', '60.0 °F is 16°C']
+                                      '50.0 °F is 10°C', 'this is a test']
 
         # self.all_calculations_list = ['10.0 °F is -12°C', '20.0 °F is -7°C',
         #                               '30.0 °F is -1°C', '40.0 °F is 4°C',
